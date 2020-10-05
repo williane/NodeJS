8370657343173e5f5e2fa168359771a36611bb44
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-10-05 16:09:15 -0300

    acessando os dados no sqlite3

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -1,3 +1,5 @@
+const db = require('../../config/database');
+
 module.exports = (app) => {
 
     app.get('/', function (req, res) {
@@ -14,20 +16,16 @@ module.exports = (app) => {
     });
 
     app.get('/livros', function (req, res) {
-        res.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    { 
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    { 
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ] 
-            }
-        );
+
+        db.all('select * from livros', function(erro, resultados){
+            res.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
+
+        
     });
 };
