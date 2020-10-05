a52cb39a661a6ce11f0de06451b695af1f567b8f
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-10-05 15:29:02 -0300

    Templates dinamicos com marko

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -12,8 +12,22 @@ module.exports = (app) => {
                  </html>    
              `);
     });
-    
+
     app.get('/livros', function (req, res) {
-        res.marko(require('../views/livros/lista/lista.marko'));
+        res.marko(
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros: [
+                    { 
+                        id: 1,
+                        titulo: 'Fundamentos do Node'
+                    },
+                    { 
+                        id: 2,
+                        titulo: 'Node Avançado'
+                    }
+                ] 
+            }
+        );
     });
 };
